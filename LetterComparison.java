57c571850391770e278fb97a1b86be1b26909df8
Shahana T P <shahanathirumurugan717@gmail.com>
2025-01-31 12:23:20 +0530

    Add files via upload
    Imagine you have two secret letters, „A‟ and „B‟. Your task is to write a program that  uses different comparison tools  to uncover how these letters relate to each other. Can you  figure out which one is greater or less than the other? Use your program to solve this  letter comparison mystery! For example, consider two characters „A‟ and „B‟.  „A‟ < „B‟ is true. „A‟ <= „B‟ is true. „A‟ > „B‟ is false.  „A‟ >= „B‟ is false.  „A‟ == „B‟ is false.  „A‟ != „B‟ is true.

---
+++ LetterComparison.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class LetterComparison {
+public static void main(String[] args) {
+char letter1 = 'A';
+char letter2 = 'B';
+System.out.println("Comparing the letters '" + letter1 + "' and '" + letter2 + "':");
+System.out.println(letter1 + " < " + letter2 + " is: " + (letter1 < letter2));
+System.out.println(letter1 + " <= " + letter2 + " is: " + (letter1 <= letter2));
+System.out.println(letter1 + " > " + letter2 + " is: " + (letter1 > letter2));
+System.out.println(letter1 + " >= " + letter2 + " is: " + (letter1 >= letter2));
+System.out.println(letter1 + " == " + letter2 + " is: " + (letter1 == letter2));
+System.out.println(letter1 + " != " + letter2 + " is: " + (letter1 != letter2));
+    }
+}
